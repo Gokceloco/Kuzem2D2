a327c5c95a6eb953e24e9f5f924898b36eb3a8d8
gokcegun <gokce@locomotive.games>
2024-11-06 20:09:28 +0300

    Ders6a

--- Assets/Scripts/Elements/Enemy.cs
+++ Assets/Scripts/Elements/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -10,9 +11,12 @@ public class Enemy : MonoBehaviour
 
     public float speed;
 
+    public TextMeshPro healthTMP;
+
     private void Start()
     {
         _currentHealth = startHealth;
+        healthTMP.text = _currentHealth.ToString();
     }
 
     void Update()
@@ -23,6 +27,7 @@ public class Enemy : MonoBehaviour
     public void GetHit(int damage)
     {
         _currentHealth -= damage;
+        healthTMP.text = _currentHealth.ToString();
         if (_currentHealth <= 0)
         {
             gameObject.SetActive(false);
