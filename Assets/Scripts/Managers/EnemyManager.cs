1ff03f2386c0e8cb8bf82d4ad53fe0d95795c177
gokcegun <gokce@locomotive.games>
2024-11-18 20:10:25 +0300

    Boss Added

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -5,24 +5,72 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     public Enemy enemyPrefab;
+    public Enemy bossEnemyPrefab;
 
     public float enemyYSpacing;
 
-    public int enemyCount;
+    private int _spawnedEnemyCount;
 
     public void StartEnemyManager()
     {
-        SpawnEnemies();
+        StartCoroutine(EnemyGenerationCoroutine());
+        _spawnedEnemyCount = 0;
+    }    
+
+    IEnumerator EnemyGenerationCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.5f + Random.Range(0,2f));
+            if (_spawnedEnemyCount < 20)
+            {
+                if (Random.value < .75f)
+                {
+                    SpawnEnemy();
+                }
+                else
+                {
+                    SpawnTwoEnemies();
+                }
+            }
+            else
+            {
+                yield return new WaitForSeconds(4);
+                SpawnBoss();
+                break;
+            }            
+        }
+    }
+    void SpawnEnemy()
+    {
+        var newEnemy = Instantiate(enemyPrefab);
+        var enemyXPos = Random.Range(-2.2f, 2.2f);
+        var enemyYPos = 5 * enemyYSpacing;
+        newEnemy.transform.position = new Vector3(enemyXPos, enemyYPos, 0);
+        _spawnedEnemyCount++;
     }
 
-    void SpawnEnemies()
+    void SpawnTwoEnemies()
     {
-        for (int i = 0; i < enemyCount; i++) 
-        {
-            var newEnemy = Instantiate(enemyPrefab);
-            var enemyXPos = Random.Range(-2.2f, 2.2f);
-            var enemyYPos = 6 + i * enemyYSpacing;
-            newEnemy.transform.position = new Vector3(enemyXPos, enemyYPos, 0);
-        }              
+        var newEnemy = Instantiate(enemyPrefab);
+        var enemyXPos = Random.Range(1f, 2.2f);
+        var enemyYPos = 5 * enemyYSpacing;
+        newEnemy.transform.position = new Vector3(enemyXPos, enemyYPos, 0);
+        _spawnedEnemyCount++;
+
+        var newEnemy2 = Instantiate(enemyPrefab);
+        var enemyXPos2 = Random.Range(-1f, -2.2f);
+        var enemyYPos2 = 5 * enemyYSpacing;
+        newEnemy2.transform.position = new Vector3(enemyXPos2, enemyYPos2, 0);
+        _spawnedEnemyCount++;
+    }
+
+    void SpawnBoss()
+    {
+        var newEnemy = Instantiate(bossEnemyPrefab);
+        var enemyXPos = Random.Range(-2f, 2f);
+        var enemyYPos = 5 * enemyYSpacing;
+        newEnemy.transform.position = new Vector3(enemyXPos, enemyYPos, 0);
+        _spawnedEnemyCount++;
     }
 }
